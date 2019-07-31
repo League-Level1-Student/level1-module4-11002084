6c7c8217b3b0e7f4220cc92ce9bd85fade3f0293
League Student <league@iMac-37.attlocal.net>
2019-07-31 16:29:33 -0700

    Whack A Mole (IP), Slot Machine (IP)

--- src/_11_whack_a_mole/WhackAMole.java
+++ src/_11_whack_a_mole/WhackAMole.java
@@ -3,33 +3,40 @@ package _11_whack_a_mole;
 import java.applet.AudioClip;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Date;
 import java.util.Random;
 
 import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class WhackAMole implements ActionListener {
-	JFrame frame = new JFrame();
+	JFrame frame;
 	Random random = new Random();
-	JButton moleButton = new JButton();
-	JPanel panel = new JPanel();
+	JPanel panel;
+	int molesWhacked;
+	int timesMissed;
+	JButton moleButton;
 
 	void createUI() {
+		frame = new JFrame();
+		panel = new JPanel();
 		frame.setSize(250, 275);
 		frame.add(panel);
 		drawButtons(21);
-		frame.setTitle("Don't Tell PETA");
+		frame.setTitle("Aneurysm Induce-inator");
 		frame.setVisible(true);
 	}
 
 	void drawButtons(int numberOfButtons) {
 		int num = random.nextInt(numberOfButtons) + 1;
 		for (int i = 1; i < numberOfButtons; i++) {
-
 			if (i == num) {
-				System.out.println(i);
+				moleButton = new JButton();
 				moleButton.setText("Mole!");
 				moleButton.addActionListener(this);
 				panel.add(moleButton);
@@ -40,20 +47,33 @@ public class WhackAMole implements ActionListener {
 		}
 	}
 
-	public static void main(String[] args) {
+	public void run() {
 		new WhackAMole().createUI();
+	}
+
+	private void endGame(Date timeAtStart, int molesWhacked) {
+		Date timeAtEnd = new Date();
+		JOptionPane.showMessageDialog(null, "Your whack rate is "
+				+ ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked) + " moles per second.");
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		if (e.getSource() == moleButton) {
-			playSound("Ding.wav");
-		} else {
-			speak("OOF you missed");
+		if (molesWhacked < 10 || timesMissed < 5) {
+			if (e.getSource() == moleButton) {
+				playSound("Ding.wav");
+				molesWhacked++;
+				System.out.println(molesWhacked);
+			} else {
+				speak("OOF you missed");
+				timesMissed++;
+			}
+		} else if (molesWhacked >= 10) {
+			
 		}
+
 		frame.dispose();
-		frame.remove(panel);
 		createUI();
 	}
 
